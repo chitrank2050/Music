d70f02457e8753c1a92151482b2c3f008995516e
chitrank2050 <chitrank2050@gmail.com>
2020-06-12 09:51:20 +0530

    Finished the code

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,7 @@ export default {
         this.player.src = this.current.src;
       }
       this.player.play();
+      this.player.addEventListener('ended', () => this.next());
       this.isPlaying = true;
     },
     pause() {
@@ -101,5 +102,83 @@ header {
   padding: 15px;
   background: #212121;
   color: #fff;
+}
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title {
+  color: #53655a;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+.play {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0 15px;
+  border-radius: 8px;
+  color: #fff;
+  background-color: #cc2e5d;
+}
+button:hover {
+  opacity: 0.8;
+}
+.next,
+.prev,
+.pause {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0 15px;
+  border-radius: 8px;
+  color: #fff;
+  background-color: #ff5858;
+}
+.playlist {
+  padding: 0 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 15px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover {
+  color: #ff5858;
+}
+.playlist .song.playing {
+  color: #fff;
+  background-image: linear-gradient(to right, #cc2e5d, #ff5858);
 }
 </style>
